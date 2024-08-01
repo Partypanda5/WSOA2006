7ef9c24e791ddbd30d2a903bb4833a8f9aadd897
partypanda5 <scoondi@hotmail.com>
2024-08-01 08:29:30 +0200

    Coding Workshop Stuff

---
+++ FirstPersonProject/Assets/Scripts/CodingWorkshop.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodingWorkshop : MonoBehaviour
+{
+    int num = 6;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int score = 5;
+        print(score);
+
+        num = num + 5;
+
+        num += 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
